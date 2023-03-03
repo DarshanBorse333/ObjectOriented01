cd9a2ac678e6ebf0c350a91a24da18c4da846f40
DarshanBorse333 <Darshanborse@gmail.com>
2023-03-03 13:46:37 +0530

    .

--- ObjectOriented01/Student.cs
+++ ObjectOriented01/Student.cs
@@ -12,14 +12,30 @@ namespace ObjectOriented01
     }
     internal class Student
     {
-        public static void Demo()
+        public int Id { get; set; }   //Property,, if there is get and set
+
+        private int id; //  Feilds,, if there is no get and set 
+        public static void Demo()  // Function,,
         {
             Console.WriteLine("");
             
         } 
+
+        public static void CalculateArea(string shape) // accepting string
+        {
+
+        }
+
+        // Behaving in different ways depending upon input recieved use of same thing for diff purposes
+        // we can see the same name function is used but for different purposes
+        public static void CalculateArea()
+        {
+
+        }
+
     }
 
-    internal class user
+    internal class user:Student // Student class can be reuse by establishing parent-child reltionship known as inheritance
     {
         public static void Demo()
         {
